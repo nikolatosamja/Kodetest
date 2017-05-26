91684cf1a2792b3b95e1379bde8a3959a614c0b0
esd <esd@it-minds.dk>
2017-05-26 09:58:34 +0200

    Updated .NET target framework to 4.6.2 because new is always better :)

--- WebShop/C#/WebShopDatabaseModel/Model1.Designer.cs
+++ WebShop/C#/WebShopDatabaseModel/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ESD\Source\Repos\Kodetest\WebShop\C#\WebShopDatabaseModel\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ESD\Documents\Repos\kodetest\WebShop\C#\WebShopDatabaseModel\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
